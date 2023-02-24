83a470222db93268521b13602a1600a77e84cbf5
shahinsha.as <shahinsha.as>
2023-02-24 10:47:59 +0530

    changes

--- src/Component/LEDTime.js
+++ src/Component/LEDTime.js
@@ -11,22 +11,10 @@ function LEDTime() {
     return () => clearInterval(interval);
   }, []);
 
-  const hours = time.getHours().toString().padStart(2, '0');
-  const minutes = time.getMinutes().toString().padStart(2, '0');
-  const seconds = time.getSeconds().toString().padStart(2, '0');
-  const date = new Date().toLocaleDateString('en-GB')
+  const date = new Date().toLocaleString('en-GB',{hour12: true})
   return (
     <div className="led-time">
-      <div className="led-digit">{hours[0]}</div>
-      <div className="led-digit">{hours[1]}</div>
-      <div className="led-separator">:</div>
-      <div className="led-digit">{minutes[0]}</div>
-      <div className="led-digit">{minutes[1]}</div>
-      <div className="led-separator">:</div>
-      <div className="led-digit">{seconds[0]}</div>
-      <div className="led-digit">{seconds[1]}</div>
-      <div className="led-separator">|</div>
-      <div className="led-digit led-date">{date}</div>
+      <div className="led-digit">{date}</div>
     </div>
   );
 }
